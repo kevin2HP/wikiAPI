ae81551a1fac0138b10219a8ebf6780dcb0fef98
kevin2HP <49846996+kevin2HP@users.noreply.github.com>
2020-08-08 10:38:50 +1000

    first commit

---
+++ app.js
@@ -0,0 +1,115 @@
+
+const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.set('view engine','ejs');
+
+mongoose.connect('mongodb://localhost:27017/wikiDB', {useNewUrlParser: true,useUnifiedTopology: true});
+articleSchema ={
+    title:String,
+    content:String
+}
+
+const Article = mongoose.model('Article',articleSchema);
+
+////////////////////////////REquest targetoing all articles
+
+app.route('/articles')
+
+.get(function (req,res) {
+    Article.find(function (err,foundArticles) {
+        if(!err){
+            res.send(foundArticles);
+        }else{
+            res.send(err);
+        }
+      });
+})
+
+
+
+
+.post(function(req,res){
+    const newArticle = new Article({
+        title:req.body.title,
+        content:req.body.content
+    });
+    newArticle.save(function (err,foundArticles) {
+        if(!err){
+            res.send('Successfully added a new article')
+        }else{
+            res.send(err);
+        }
+      });
+ })
+ 
+ .delete(function (req,res) {
+    Article.deleteMany(function (err) {
+        if(!err){
+            res.send('Successfully Deleted all articles.')
+        }else{
+            res.send(err);
+        }
+      });
+  });
+
+////////////////////////////REquest targetoing a specific article
+
+app.route('/articles/:articleTitle')
+.get(function(req,res){
+
+Article.findOne({title:req.params.articleTitle},function (err,foundArticle) {
+    if (foundArticle){
+        res.send(foundArticle);
+    }else{
+        res.send('No articles matching that title was found');
+    }
+  });
+})
+
+.put(function(req,res){
+    Article.update(
+        {title:req.params.articleTitle}, //to be replaced
+        {title:req.body.title,content:req.body.content}, //with this from postman 
+        {overwrite:true},
+        function (err) {
+            if(!err){
+                res.send('Successfully updated article.');
+            }
+          }
+    );
+})
+
+.patch(function (req,res) {
+    Article.update(
+        {title: req.params.articleTitle}, //same as put method reallly but more accurate
+        {$set:req.body},
+        function (err) {
+            if(!err){
+                res.send('Successfully updated articles.')
+            }else{
+               res.send(err);
+            }
+          }
+    );
+  })
+
+.delete(function (req,res) {
+    Article.deleteOne(
+        {title:req.params.articleTitle},
+        function (err) {
+            if(!err){
+                res.send('Successfully deleted the corresponding article')
+            }else{
+                res.send(err);
+            }
+          }
+    );
+  });
+app.listen(3000,function () {
+    console.log('Server started on port 3000');
+    
+  })
